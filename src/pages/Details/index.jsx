bdcb39a94974c65512d3e45557192bf585aca547
gabriel@desktop <gabrieljoserporto@gmail.com>
2023-05-29 10:47:53 -0300

    add: Page Details Finished

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,35 +1,47 @@
-import { Container, Links } from "./styles"
+import { Container, Links, Content } from "./styles"
 import { Header } from "../../Components/Header"
 import { Button } from "../../Components/Button"
 import { Section } from "../../Components/Section"
 import { Tag } from "../../Components/Tag"
 import { ButtonText } from "../../Components/ButtonText"
 
-
 export function Details() {
   return (
     <Container>
       <Header />
 
-      <ButtonText title="Excluir Nota"/>
+      <main>
+        <Content>
+          <ButtonText title="Excluir nota" />
 
-      <Section title="Links úteis">
-        <Links>
-          <li>
-            <a href="#">https://www.rocketseat.com.br/</a>
-          </li>
-          <li>
-            <a href="#">https://www.rocketseat.com.br/</a>
-          </li>
-        </Links>
-      </Section>
+          <h1>Introdução ao React</h1>
 
-      <Section title="Marcadores">
-        <Tag title="express" />
-        <Tag title="nodejs" />
-      </Section>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit
+            incidunt sequi temporibus neque voluptatum impedit fugit
+            dignissimos. Aperiam libero, consectetur ea iste voluptatem
+            delectus! Quasi voluptatum facilis unde quos porro.
+          </p>
 
-      <Button title="Voltar" />
+          <Section title="Links úteis">
+            <Links>
+              <li>
+                <a href="#">https://www.rocketseat.com.br/</a>
+              </li>
+              <li>
+                <a href="#">https://www.rocketseat.com.br/</a>
+              </li>
+            </Links>
+          </Section>
+
+          <Section title="Marcadores">
+            <Tag title="express" />
+            <Tag title="nodejs" />
+          </Section>
+
+          <Button title="Voltar" />
+        </Content>
+      </main>
     </Container>
   )
 }
