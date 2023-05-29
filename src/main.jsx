31164ca2964b8d80ba3943812458cd1b5f4bdace
gabriel@desktop <gabrieljoserporto@gmail.com>
2023-05-29 18:32:47 -0300

    add: Finished Home Page

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,16 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import { Details } from "./pages/Details/index.jsx"
 import GlobalStyles from "./styles/global"
 import { ThemeProvider } from "styled-components"
 import theme from "./styles/theme"
+
+import { Home } from "./pages/Home"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-        <Details />
+        <Home />
     </ThemeProvider>
   </React.StrictMode>
 )
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,55 @@
+import { FiPlus, FiSearch } from "react-icons/fi"
+import { Container, Brand, Menu, Search, Content, NewNote } from "./styles"
+import { Header } from "../../Components/Header"
+import { ButtonText } from "../../Components/ButtonText"
+import { Input } from "../../Components/Input"
+import { Section } from "../../Components/Section"
+import { Note } from "../../Components/Note"
+
+export function Home() {
+  return (
+    <Container>
+      <Brand>
+        <h1>Rocketnotes</h1>
+      </Brand>
+
+      <Header />
+
+      <Menu>
+        <li>
+          <ButtonText title="Todos" isActive />
+        </li>
+        <li>
+          <ButtonText title="React" />
+        </li>
+        <li>
+          <ButtonText title="Nodejs" />
+        </li>
+      </Menu>
+
+      <Search>
+        <Input placeholder="Pesquisar pelo título" icon={FiSearch} />
+      </Search>
+
+      <Content>
+        <Section title="Minhas Notas">
+
+        <Note
+            data={{
+              title: "React",
+              tags: [
+                { id: "1", name: "React" },
+                { id: "2", name: "Nodejs" },
+              ],
+            }}
+          /> 
+        </Section>
+      </Content>
+
+      <NewNote>
+        <FiPlus />
+        Criar nota
+      </NewNote>
+    </Container>
+  )
+}
